41025ded2907838d1b72a37941f6dd91cea62bf9
Daniele Penazzo <penazarea@altervista.org>
2019-02-07 22:57:22 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
 #!/usr/bin/env python3
 import requests
-from bs4 import BeautifulSoup
+from datetime import date
+from datetime import timedelta
+import json
 
 useragent = "Mozilla/5.0 (X11; U; Linux i686; en-gb) AppleWebKit/525.1+ (KHTML, like Gecko, Safari/525.1+) epiphany-webkit"
-url = "https://www.packtpub.com/packt/offers/free-learning"
+today = date.today().strftime("%Y-%m-%d") + "T00:00:00.000Z"
+tomorrow = (date.today() + timedelta(days=1)).strftime("%Y-%m-%d") + "T00:00:00.000Z"
+firsturl = "https://services.packtpub.com/free-learning-v1/offers?dateFrom={}&dateTo={}".format(today, tomorrow)
 
-response = requests.get(url, headers={"user-agent": useragent})
+response = requests.get(firsturl, headers={"user-agent": useragent})
+resp1 = json.loads(response.content)
+bookid = resp1["data"][0]["productId"]
 
-soup = BeautifulSoup(response.content, "lxml")
+finalurl = "https://static.packt-cdn.com/products/{}/summary".format(bookid)
+response = requests.get(finalurl, headers={"user-agent": useragent})
+resp1 = json.loads(response.content)
+rawtitle = resp1["title"]
 
-rawtitle = soup.findAll("div", class_="dotd-title")
-
-title = rawtitle[0].text.strip()
+title = rawtitle.strip()
 
 title = title.replace("#", "\#")
 
