908fb93096d6af84cdd858b53d98d94d51445c65
Penaz <penazarea@altervista.org>
2022-08-16 20:47:13 +0200

    Update for new packt website

--- README.md
+++ README.md
@@ -2,6 +2,10 @@
 
 ![Status:Active](https://img.shields.io/badge/Project_Status-Active-brightgreen.svg)
 
-Small script, meant to be run at boot, that creates a .packt file containing today's Free Learning Ebook's title. The file can be used in Conky for Displaying on your desktop via the "cat" command
+Small script, meant to be run at boot, that by default creates a .packt file containing today's Free Learning Ebook's title. The file can be used in Conky for Displaying on your desktop via the "cat" command
 
-The "Notifier" Branch allows to have a version that uses a "Notify-Send" subprocess to show a notification on desktop
+Helpful arguments:
+
+- `--conky`: Does some small escaping to avoid possible issues in Conky Configurations;
+- `--notify`: Uses Notify-Send to send a notification instead of creating a `~/.packt` File;
+- `--api`: Uses the api-based scraping approach (legacy);
--- main.py
+++ main.py
@@ -1,29 +1,95 @@
 #!/usr/bin/env python3
+import re
 import requests
+import argparse
 from datetime import date
 from datetime import timedelta
+from subprocess import Popen
 import json
+from pathlib import Path
+from os.path import join as pathjoin
 
-useragent = "Mozilla/5.0 (X11; U; Linux i686; en-gb) AppleWebKit/525.1+ (KHTML, like Gecko, Safari/525.1+) epiphany-webkit"
-today = date.today().strftime("%Y-%m-%d") + "T00:00:00.000Z"
-tomorrow = (date.today() + timedelta(days=1)).strftime("%Y-%m-%d") + "T00:00:00.000Z"
-firsturl = "https://services.packtpub.com/free-learning-v1/offers?dateFrom={}&dateTo={}".format(today, tomorrow)
+USERAGENT = "Mozilla/5.0 (X11; U; Linux i686; en-gb) AppleWebKit/525.1+ (KHTML, like Gecko, Safari/525.1+) epiphany-webkit"
+REGEX = re.compile(r"""<h3 class="product-info__title">(?P<content>.*)</h3>""")
 
-response = requests.get(firsturl, headers={"user-agent": useragent})
-resp1 = json.loads(response.content)
-bookid = resp1["data"][0]["productId"]
 
-finalurl = "https://static.packt-cdn.com/products/{}/summary".format(bookid)
-response = requests.get(finalurl, headers={"user-agent": useragent})
-resp1 = json.loads(response.content)
-rawtitle = resp1["title"]
+def api_based():
+    """
+    Uses the legacy API-based approach to get the ebook title
+    """
+    today = date.today().strftime("%Y-%m-%d") + "T00:00:00.000Z"
+    tomorrow = (date.today() + timedelta(days=1)).strftime("%Y-%m-%d") + "T00:00:00.000Z"
+    firsturl = "https://services.packtpub.com/free-learning-v1/offers?dateFrom={}&dateTo={}".format(today, tomorrow)
 
-title = rawtitle.strip()
+    response = requests.get(firsturl, headers={"user-agent": USERAGENT})
+    resp1 = json.loads(response.content)
+    bookid = resp1["data"][0]["productId"]
 
-title = title.replace("#", "\#")
+    finalurl = "https://static.packt-cdn.com/products/{}/summary".format(bookid)
+    response = requests.get(finalurl, headers={"user-agent": USERAGENT})
+    resp1 = json.loads(response.content)
+    rawtitle = resp1["title"]
 
-if title == "":
-    title = "No title found - Check Packt's Free Learning Page"
+    title = rawtitle.strip()
 
-with open("/home/penaz/.packt", "w") as p:
-    p.write(title)
+    if title == "":
+        title = "No title found - Check Packt's Free Learning Page"
+
+    return title
+
+
+def scraper():
+    """
+    Uses a regex-based web scraper to get the ebook title
+    """
+    url = "https://www.packtpub.com/free-learning"
+    response = requests.get(url, headers={"user-agent": USERAGENT})
+
+    content = response.text
+
+    title = REGEX.search(content).groupdict().get(
+        "content", "Could not scrape title, check the PacktPub Website"
+    )
+
+    return title
+
+
+def write_file(title):
+    """
+    Writes a file in home to be used with Conky
+    """
+    with open(pathjoin(Path.home(), ".packt"), "w") as p:
+        p.write(title)
+
+
+def notify(title):
+    """
+    Sends the title as a notification with Notify-Send
+    """
+    Popen(["notify-send", "Today @ Packt", title])
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--api", help="Use api-based approach (legacy)", action="store_true"
+    )
+    parser.add_argument(
+        "--notify", help="Use notify-send instead of .packt file",
+        action="store_true"
+    )
+    parser.add_argument(
+        "--conky", help="Escape characters to avoid issues with Conky",
+        action="store_true"
+    )
+    args = parser.parse_args()
+    if args.api:
+        title = api_based()
+    else:
+        title = scraper()
+    if args.conky:
+        title = title.replace("#", "\#")
+    if args.notify:
+        notify(title)
+    else:
+        write_file(title)
